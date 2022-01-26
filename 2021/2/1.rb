663291454cfc537d1553498825f5f2aea4582976
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2022-01-26 23:26:09 +0300

    Fix 2/1

--- 2021/2/1.rb
+++ 2021/2/1.rb
@@ -1,3 +1,15 @@
-i = File.read('input.txt')
+def curs(input)
+  input
+    .split #разделение на отдельные обьекты в массиве [a b c d] -> ['a', 'b', 'c', 'd']
+    .each_slice(2) #групировка внутри масивва на массивы с 2 обьктами ['a', 'b', 'c', 'd'] -> [['a', 'b'], ['c', 'd']]
+    .group_by(&:first) #групировка в хеши с ключом 1 именни в массиве [['a', 'b'], ['c', 'd']] -> #=> {a=> [a, b], c=> [c, d]}
+    .transform_values do |x|
+       x.map do |s|
+         s.last.to_i #последний обьект в массиве переводим в integer
+       end.sum #сумма в каждом ключе
+      end.then { |s| (s['forward'] * (s['down'] - s['up']) ) } 
+end
 
-p i.split.each_slice(2).group_by(&:first).transform_values { |x| x.map{ |s| s.last.to_i }.sum }.then { |s| (s['forward'] * (s['down'] - s['up']) ) }
+#irb  input.split.each_slice(2).group_by(&:first).transform_values { |x| x.map{ |i| i.last.to_i }.sum }.then { |s| (s['forward'] * (s['down'] - s['up']) ) }
+
+puts curs File.read('input.txt')
