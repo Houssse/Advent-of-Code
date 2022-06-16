b799b43de2c459252fc194108144505f04444c5a
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2022-06-16 04:21:41 +0300

    luz day 1/2 :c

--- 2019/day 1/2.rb
+++ 2019/day 1/2.rb
@@ -3,7 +3,11 @@ input = File.read('input.txt').split.map(&:to_i)
 fuel = 0
 
 input.each do |mass|
-  fuel += mass / 3 - 2
+  fuel += mass
+  while mass > 0
+    mass = mass / 3 - 2
+    fuel += mass
+  end
 end
 
 puts fuel
