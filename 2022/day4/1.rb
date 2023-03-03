07b21b60cad2240ebe3db65ddbd758b5bffcba31
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2023-03-03 03:35:10 +0300

    end da4/2

--- 2022/day4/1.rb
+++ 2022/day4/1.rb
@@ -1,3 +1,5 @@
 input = File.read('input.txt').lines.map {|l| l.scan(/\d+/).map(&:to_i) }
 
 p input.map {|a| [(a[0]..a[1]), (a[2]..a[3])].permutation.any? {|a,b| a.cover? b } }.count(true)
+
+p input.map {|a| a[0] > a[3] || a[1] < a[2] }.count(false)
