7c4890ec80497cea312e0f549e2b7c59d0af6842
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-01-17 14:17:58 +0300

    removing unnecessaye

--- 2018/day 1/1.rb
+++
@@ -1,3 +0,0 @@
-input = File.read('input.txt').split.map(&:to_i).sum
-
-puts input
--- 2019/day 1/1.rb
+++
@@ -1,9 +0,0 @@
-input = File.read('input.txt').split.map(&:to_i)
-
-fuel = 0
-
-input.each do |mass|
-  fuel += mass / 3 - 2
-end
-
-puts fuel
