77970be1d14efe25cfa42093c549d4dc20e50aa2
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-12-12 10:57:32 +0300

    finish day1

---
+++ 2024/day1/main.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Part 1
+input = File.read('input.txt').split("\n").map { |row| row.split.map(&:to_i) }.transpose
+
+left_column = input[0]
+right_column = input[1]
+
+p left_column.sort.zip(right_column.sort).map { |a, b| (a - b).abs }.sum
+
+
+
+# Part 2
+
+p left_column.map { |i| [i, right_column.count(i)] }.map { |arr| arr.inject(&:*) }.sum
